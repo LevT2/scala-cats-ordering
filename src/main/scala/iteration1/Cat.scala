1d4fbc586ec54bf17cd10251fbf2563f199a6992
Anton Semenov <tochka313@gmail.com>
2020-03-22 07:58:03 +0900

    Delete cat examples

--- src/main/scala/iteration1/Cat.scala
+++ src/main/scala/iteration1/Cat.scala
@@ -1,18 +1,5 @@
 package iteration1
 
-object Cat {
-
-  val values: List[Cat] =
-    List(
-      Cat(8, "Hana", available = true),
-      Cat(4, "Momo", available = false),
-      Cat(2, "Shiro", available = true),
-      Cat(2, "Fuku", available = true),
-      Cat(9, "Kuro", available = false),
-      Cat(7, "Kuro", available = false)
-    )
-}
-
 case class Cat(age: Int,
                name: String,
                available: Boolean)
